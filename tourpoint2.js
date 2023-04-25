454fd923663289e2017a86c123c93e2cf6bd1638
meownoi1 <124670798+meownoi1@users.noreply.github.com>
2023-04-25 15:29:29 +0700

    Add files via upload

---
+++ tourpoint2.js
@@ -0,0 +1,155 @@
+var tourpoint = {
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "เมืองชัยภูมิ",
+          "type": "พิเศษ",
+          "level": 8
+        },
+        "geometry": {
+          "coordinates": [
+            102.0325503883056,
+            15.814067483117356
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "น้ำตกตาดโตน",
+          "type": "ธรรมชาติ",
+          "level": 9
+        },
+        "geometry": {
+          "coordinates": [
+            101.99685658441842,
+            15.974710951535712
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "อุทยานแห่งชาติภูแลนคา",
+          "type": "ธรรมชาติ",
+          "level": 9
+        },
+        "geometry": {
+          "coordinates": [
+            101.96631827116079,
+            16.063957619823682
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "บ้านเขว้า",
+          "type": "วัฒนธรรม",
+          "level": 6
+        },
+        "geometry": {
+          "coordinates": [
+            101.904782481236,
+            15.782386308593502
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "ทุ่งดอกกระเจียว",
+          "type": "ธรรมชาติ",
+          "level": 9
+        },
+        "geometry": {
+          "coordinates": [
+            101.3894676654919,
+            15.633376643371818
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "บึงละหาน",
+          "type": "ธรรมชาติ",
+          "level": 7
+        },
+        "geometry": {
+          "coordinates": [
+            101.90122507336525,
+            15.593684870901981
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "วัดศิลาอาสน์",
+          "type": "วัฒนธรรม",
+          "level": 4
+        },
+        "geometry": {
+          "coordinates": [
+            102.06053850704939,
+            15.916288178302764
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "ทุ่งกังหันลมซับใหญ่",
+          "type": "ธรรมชาติ",
+          "level": 5
+        },
+        "geometry": {
+          "coordinates": [
+            101.63064405769353,
+            15.625760887797341
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "วันพระธาตุชัยภูมิ",
+          "type": "วัฒนธรรม",
+          "level": 6
+        },
+        "geometry": {
+          "coordinates": [
+            102.22385176537739,
+            16.116066499715444
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Pine camp แคมป์สน  ",
+          "type": "ธรรมชาติ",
+          "level": 7
+        },
+        "geometry": {
+          "coordinates": [
+            102.05269091534569,
+            16.058976620339124
+          ],
+          "type": "Point"
+        }
+      }
+    ]
+  }
